9cafc5f0c8cd8b4d4aef79308318ce8b7a68e4c3
eduard.hannoshyn <eduard.hannoshyn@avenga.com>
2024-02-16 11:22:24 +0200

    added demo link to description

--- web-components/src/main.js
+++ web-components/src/main.js
@@ -1,5 +1,5 @@
-import BoxImage from "../components/box-image.js";
-import ThreeDBox from "../components/3d-box.js";
+import BoxImage from "./components/box-image.js";
+import ThreeDBox from "./components/3d-box.js";
 
 customElements.define("box-image", BoxImage);
 customElements.define("three-d-box", ThreeDBox);
